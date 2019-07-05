fe19c722e2a814fd6a34bd4607c87807e2fae87f
Sonwabo Zuma <sonwabilestarzuma@outlook.com>
2019-07-05 14:51:22 +0200

    my Database strored procedures

---
+++ dbo.Procedure.sql
@@ -0,0 +1,29 @@
+﻿
+--CREATE PROCEDURE [dbo].[GetEmployees] 
+--AS
+--BEGIN
+--	SET NOCOUNT ON;
+--	SELECT AllEmployees FROM Employees
+--END
+--GO
+
+--CREATE PROCEDURE [dbo].[DeleteEmployeeById] 
+--	@id int  
+--AS
+--BEGIN
+--	SET NOCOUNT ON;
+--	DELETE FROM Employees WHERE [Id] = @id
+--END
+--GO
+
+--CREATE PROCEDURE [dbo].[UpdateEmployeeById]
+--@email nvarchar(256),
+--@id int
+
+--AS
+--BEGIN
+--	SET NOCOUNT ON;
+--	UPDATE Employees SET [Email] = @email WHERE [Id] = @id
+--END
+
+--GO
